4e4d0772fe1b9d8729ac966e79fa8892cba0febe
Amanj2000 <amanj13508@gmail.com>
2023-02-01 15:38:04 +0530

    added Player interface

---
+++ src/Player.java
@@ -0,0 +1,44 @@
+abstract public class Player {
+    private String name;
+    private int age;
+    private int totalRun;
+    private int ballsPlayed;
+
+    Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.totalRun = 0;
+        this.ballsPlayed = 0;
+    }
+
+    abstract public boolean isOut();
+    abstract public void display();
+
+    private int scoreRun() {
+        return (int)(Math.random() * 7);
+    }
+
+    public boolean bat() {
+        if(isOut()) return false;
+
+        totalRun += scoreRun();
+        ballsPlayed += 1;
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getTotalRun() {
+        return totalRun;
+    }
+
+    public int getBallsPlayed() {
+        return ballsPlayed;
+    }
+}
