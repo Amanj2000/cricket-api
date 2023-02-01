0fccc9d3f4d26f5833e21199939b1e07bbf5a233
Amanj2000 <amanj13508@gmail.com>
2023-02-01 15:58:38 +0530

    added Match class

---
+++ src/Match.java
@@ -0,0 +1,28 @@
+public class Match {
+    private Team team1;
+    private Team team2;
+
+    public Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    public void startMatch() {
+        System.out.println("Team batting: " + team1.getName());
+        team1.play();
+        System.out.println();
+        System.out.println("Team batting: " + team2.getName());
+        team2.play();
+        System.out.println();
+    }
+
+    public void displayResult() {
+        if(team1.getTeamScore() > team2.getTeamScore()) {
+            System.out.printf("%s won!\n", team1.getName());
+        } else if(team1.getTeamScore() < team2.getTeamScore()) {
+            System.out.printf("%s won!\n", team2.getName());
+        } else {
+            System.out.printf("Draw\n");
+        }
+    }
+}
